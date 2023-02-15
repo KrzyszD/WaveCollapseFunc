f449c63f9269b5f656e832d2ea567d100593afa9
kdutka2 <kdutka2@illinois.edu>
2023-02-15 15:30:59 -0600

    Added description to mode

--- config.py
+++ config.py
@@ -2,6 +2,11 @@
 width = 40
 height = 20
 
+# mode  : description
+# 0     : Rectangular rooms only
+# 1     : Non-rectangular rooms
+# 2     : Walls have thickness
+# 3     : Pillars have thickness
 mode = 2
 
 
